e1bf0e7403dc6e7acfd928bccdd2fe6a50562e90
prassee <prassee>
2013-08-12 16:57:15 +0530

    updated code to handle params
    Signed-off-by: prassee <prassee>

--- netis-server/src/main/scala/netis/NetisServer.scala
+++ netis-server/src/main/scala/netis/NetisServer.scala
@@ -12,8 +12,10 @@ import io.netty.buffer.UnpooledUnsafeDirectByteBuf
 import io.netty.buffer.EmptyByteBuf
 import io.netty.handler.codec.marshalling.ChannelBufferByteOutput
 import java.net.InetSocketAddress
+import scala.collection.mutable.Map
 
 object NetisServerCommons {
+
   val resolveQueryString = (msg: Object) => {
     val byteBuf = msg.asInstanceOf[UnpooledUnsafeDirectByteBuf]
     val reqStr = new StringBuilder
@@ -23,23 +25,43 @@ object NetisServerCommons {
     byteBuf.release()
     reqStr.toString()
   }
-}
 
-class NetisRequestHandler(handler: String => String)
-  extends ChannelInboundHandlerAdapter {
-  import netis.NetisServerCommons._
-  override def channelRead(ctx: ChannelHandlerContext, msg: Object) {
-    ctx.writeAndFlush(ctx.alloc().buffer().writeBytes(
-      handler(resolveQueryString(msg)).getBytes()))
+  val obtainRequest = (url: String) => url.split("/")(0)
+
+  val obtainRequestParam = (url: String) => {
+    val buffer = Map[String, String]()
+    url.split("/")(1).split("&").foreach(str => {
+      buffer.+=((str.split("=")(0), str.split("=")(1)))
+    })
+    buffer
   }
 }
 
+object HandlerType {
+  type handlerDel = (String, Map[String, String]) => String
+}
+
+import netis.HandlerType._
+
+class NetisRequestHandler(handler: handlerDel)
+  extends ChannelInboundHandlerAdapter {
+
+  import netis.NetisServerCommons.{ resolveQueryString, obtainRequest, obtainRequestParam }
+
+  override def channelRead(ctx: ChannelHandlerContext, msg: Object) {
+    val reqUrl = resolveQueryString(msg)
+    ctx.writeAndFlush(ctx.alloc().buffer().writeBytes(
+      handler(obtainRequest(reqUrl), obtainRequestParam(reqUrl)).getBytes()))
+  }
+
+}
+
 object NetisServer {
-  def apply(port: Int, handlr: String => String) =
+  def apply(port: Int, handlr: handlerDel) =
     new NetisServer(host = "localhost", port, handlr)
 }
 
-class NetisServer(host: String, port: Int, handlr: String => String) {
+class NetisServer(host: String, port: Int, handlr: handlerDel) {
 
   val boss = new NioEventLoopGroup
   val worker = new NioEventLoopGroup
@@ -48,8 +70,8 @@ class NetisServer(host: String, port: Int, handlr: String => String) {
     .group(boss, worker)
     .channel(classOf[NioServerSocketChannel])
     .childHandler(new ChannelInitializer[SocketChannel]() {
-      override def initChannel(chnnel: SocketChannel) {
-        chnnel.pipeline().addLast(new NetisRequestHandler(handlr))
+      override def initChannel(channel: SocketChannel) {
+        channel.pipeline().addLast(new NetisRequestHandler(handlr))
       }
     })
 
--- netis-server/src/test/scala/netis/NetisBootStrap.scala
+++ netis-server/src/test/scala/netis/NetisBootStrap.scala
@@ -1,10 +1,17 @@
 package netis
 
+import scala.collection.mutable.Map
+
 object NetisServerApp extends App {
-  NetisServer(9090, (url: String) => {
+
+  NetisServer(9090, (url, params) => {
     url match {
-      case "java" => "returning java"
+      case "java" => {
+        "returning java"
+      }
       case "scala" => "returning scala"
     }
   }).run
+
 }
+
